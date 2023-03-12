29800860463cc272b30518740090bc6e04cdeb8f
feranmiodugbemi10 <feranmiodugbemi@gmail.com>
2023-03-12 22:18:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+
+app.post('/', (req, res)=>{
+    console.log(req.body)
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+  });
